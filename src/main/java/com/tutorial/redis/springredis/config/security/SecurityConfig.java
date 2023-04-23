3ab0d836fc78d185abbb463f4adabde5572c4343
aaa <cnj991279@gmail.com>
2023-04-23 20:38:26 +0900

    Update SecurityConfig.java

--- src/main/java/com/tutorial/redis/springredis/config/security/SecurityConfig.java
+++ src/main/java/com/tutorial/redis/springredis/config/security/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+// 필터 부분에서 오래된 내용이라 구현 못한 거 같음
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
